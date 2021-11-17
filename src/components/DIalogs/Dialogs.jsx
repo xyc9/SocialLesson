8f8c6132fd402dbc0c7a86882e4a7c70b2eff238
xyc9 <od.admin@partner.bg-corp.net>
2021-11-17 16:00:51 +0300

    End 23 Lesson

--- src/components/DIalogs/Dialogs.jsx
+++ src/components/DIalogs/Dialogs.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 
-const Dialogs = () =>{
-    return(
-        <div>
+const UserDialog = (props) => {
+    return (
+        <p>{props.message}</p>
+    )
+}
+
+const UserItem = (props) => {
+    let Link = "/messages/" + props.id;
+    return (
+        <NavLink to={Link}>{props.username}</NavLink>
+    )
+}
+
+
+const Dialogs = () => {
+
+
+    return (
+        <div className="Dialogs_page">
             <div className="Dialogs_UserList">
                 <ul>
-                    <li><NavLink to='/messages/1'>user1</NavLink></li>
-                    <li><NavLink to='/messages/2'>user1</NavLink></li>
-                    <li><NavLink to='/messages/3'>user1</NavLink></li>
+                    <li><UserItem id="1" username="user1"/></li>
+                    <li><UserItem id="2" username="user2"/></li>
+                    <li><UserItem id="3" username="user3"/></li>
 
+
+                </ul>
+            </div>
+            <div className="Dialogs_message">
+                <ul>
+                    <li>
+                        <UserDialog message="first message"/>
+
+                    </li>
+                    <li>
+                        <UserDialog message="second message"/>
+                    </li>
+                    <li>
+                        <UserDialog message="third message"/>
+                    </li>
                 </ul>
             </div>
         </div>
--- src/components/Profile/MyInfo.jsx
+++ src/components/Profile/MyInfo.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const MyInfo = () =>{
-    return<>
+    return(
         <div className="profile_info">
             <img src='https://avatarfiles.alphacoders.com/792/thumb-1920-79295.jpg' alt=""/>
             name \ surname
         </div>
-
-     </>
+    )
 }
 export default MyInfo;
